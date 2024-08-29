85604dd0392b5e3902f04f31d55ab10133e0ad9d
Filip Mårtensson <fenwikk@protonmail.com>
2024-08-29 21:53:41 +0200

    all joes are the same, theyre rotting my brain

--- apps/app/src/routes/(app)/(root)/+page.svelte
+++ apps/app/src/routes/(app)/(root)/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-
 	type Status = 'hanging' | 'down' | 'ghost';
 	type Friend = {
 		name: string;
@@ -8,82 +6,71 @@
 		status: Status;
 	};
 
-	let friends: Friend[] | undefined = undefined;
-
-	onMount(async () => {
-		friends = [
-			{
-				name: 'John Doe',
-				avatar: await fetch('https://thispersondoesnotexist.com', {
-					mode: 'cors'
-				})
-					.then((res) => res.blob())
-					.then((blob) => URL.createObjectURL(blob)),
-				status: 'hanging'
-			},
-			{
-				name: 'John Doe',
-				avatar: await fetch('https://thispersondoesnotexist.com', {
-					mode: 'cors'
-				})
-					.then((res) => res.blob())
-					.then((blob) => URL.createObjectURL(blob)),
-				status: 'hanging'
-			},
-			{
-				name: 'John Doe',
-				avatar: await fetch('https://thispersondoesnotexist.com', {
-					mode: 'cors'
-				})
-					.then((res) => res.blob())
-					.then((blob) => URL.createObjectURL(blob)),
-				status: 'hanging'
-			},
-			{
-				name: 'John Doe',
-				avatar: await fetch('https://thispersondoesnotexist.com', {
-					mode: 'cors'
-				})
-					.then((res) => res.blob())
-					.then((blob) => URL.createObjectURL(blob)),
-				status: 'down'
-			},
-			{
-				name: 'John Doe',
-				avatar: await fetch('https://thispersondoesnotexist.com', {
-					mode: 'cors'
-				})
-					.then((res) => res.blob())
-					.then((blob) => URL.createObjectURL(blob)),
-				status: 'ghost'
-			},
-			{
-				name: 'John Doe',
-				avatar: await fetch('https://thispersondoesnotexist.com', {
-					mode: 'cors'
-				})
-					.then((res) => res.blob())
-					.then((blob) => URL.createObjectURL(blob)),
-				status: 'ghost'
-			}
-		];
-	});
+	const friends: Friend[] = [
+		{
+			name: 'John Doe',
+			avatar: 'https://thispersondoesnotexist.com/',
+			status: 'hanging'
+		},
+		{
+			name: 'John Doe',
+			avatar: 'https://thispersondoesnotexist.com/',
+			status: 'hanging'
+		},
+		{
+			name: 'John Doe',
+			avatar: 'https://thispersondoesnotexist.com/',
+			status: 'hanging'
+		},
+		{
+			name: 'John Doe',
+			avatar: 'https://thispersondoesnotexist.com/',
+			status: 'down'
+		},
+		{
+			name: 'John Doe',
+			avatar: 'https://thispersondoesnotexist.com/',
+			status: 'ghost'
+		},
+		{
+			name: 'John Doe',
+			avatar: 'https://thispersondoesnotexist.com/',
+			status: 'ghost'
+		}
+	] as const;
 </script>
 
-<h1 class="text-5xl font-black">Good morning!</h1>
+<div class="flex flex-col gap-8">
+	<h1 class="text-5xl font-black">Good morning!</h1>
 
-<div class="flex flex-col gap-5">
-	{#if friends === undefined}
-		<p>Loading...</p>
-	{:else}
+	<div class="flex flex-col gap-5">
 		{#each friends as friend}
-			<div class="flex items-center space-x-4">
-				<img src={friend.avatar} alt={friend.name} class="w-12 h-12 rounded-full" />
+			<div class="flex items-center gap-3">
+				<img src={friend.avatar} alt={friend.name} class="w-12 h-12 rounded-2xl" />
 				<div>
-					<p class="text-lg font-semibold">{friend.name}</p>
-					<p class="text-sm text-gray-500">{friend.status}</p>
+					<p class="text-2xl font-black">{friend.name}</p>
+					<p
+						class="text-sm flex items-center gap-1.5 {friend.status === 'hanging'
+							? 'text-green-500'
+							: friend.status === 'down'
+								? 'text-yellow-500'
+								: 'text-gray-500'}"
+					>
+						<span
+							class="w-2 h-2 rounded-sm block {friend.status === 'hanging'
+								? 'bg-green-500'
+								: friend.status === 'down'
+									? 'bg-yellow-500'
+									: 'bg-gray-500'}"
+						/>
+
+						{friend.status}
+						{#if friend.status === 'hanging'}
+							<span class="text-gray-500">2 km</span>
+						{/if}
+					</p>
 				</div>
 			</div>
 		{/each}
-	{/if}
+	</div>
 </div>
